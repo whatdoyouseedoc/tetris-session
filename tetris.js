58281838e4fd3d2800ada425c5905fc1b5427afe
Alexander <whatdoyouseedoc@gmail.com>
2018-02-02 22:56:53 +0300

    Implemented tetris. DOTO: refactor via OOP approach.

--- tetris.js
+++ tetris.js
@@ -5,11 +5,23 @@ context.fillStyle = '#000';
 context.fillRect(0, 0, canvas.width, canvas.height);
 context.scale(20, 20);
 
-const matrix = [
-    [0, 0, 0],
-    [1, 1, 1],
-    [0, 1, 0]
-];
+function arenaSweep() {
+    let rowCount = 1;
+    
+    outer: for (let y = arena.length - 1; y > 0 ; --y) {
+        for (let x = 0; x < arena[y].length; ++x) {
+            if (arena[y][x] === 0) {
+                continue outer;
+            }
+        }
+
+        const row = arena.splice(y, 1)[0].fill(0);
+        arena.unshift(row);
+        ++y;
+        player.score += rowCount * 10;
+        rowCount *= 2;
+    }
+}
 
 function createMatrix(w, h) {
     const matrix = [];
@@ -17,6 +29,57 @@ function createMatrix(w, h) {
         matrix.push(new Array(w).fill(0));
     }
     return matrix;
+}
+
+const pieces = {
+    'T': [
+        [0, 0, 0],
+        [1, 1, 1],
+        [0, 1, 0]
+    ],
+    'L': [
+        [0, 2, 0],
+        [0, 2, 0],
+        [0, 2, 2]
+    ],
+    'J': [
+        [0, 3, 0],
+        [0, 3, 0],
+        [3, 3, 0]
+    ],
+    'O': [
+        [4, 4],
+        [4, 4]
+    ],
+    'S': [
+        [0, 5, 5],
+        [5, 5, 0],
+        [0, 0, 0]
+    ],
+    'Z': [
+        [6, 6, 0],
+        [0, 6, 6],
+        [0, 0, 0]
+    ],
+    'I': [
+        [0, 7, 0, 0],
+        [0, 7, 0, 0],
+        [0, 7, 0, 0],
+        [0, 7, 0, 0]
+    ],
+};
+
+function createPiece(type) {
+    switch(type) {
+        case 'T': return pieces.T; break;
+        case 'L': return pieces.L; break;
+        case 'J': return pieces.J; break;
+        case 'O': return pieces.O; break;
+        case 'S': return pieces.S; break;
+        case 'Z': return pieces.Z; break;
+        case 'I': return pieces.I; break;
+        default: return pieces.T; break;
+    }
 }
 
 const arena = createMatrix(12, 20);
@@ -60,7 +123,7 @@ function drawMatrix(matrix, offset) {
     matrix.forEach((row, y) => {
         row.forEach((value, x) => {
             if (value !== 0) {
-                context.fillStyle = 'red';
+                context.fillStyle = colors[value];
                 context.fillRect(x + offset.x, y + offset.y, 1, 1);
             }
         });
@@ -74,12 +137,61 @@ function playerMove(dir) {
     }
 }
 
+function playerReset() {
+    const pcs = 'OILJSZT';
+    player.matrix = createPiece(pcs[pcs.length * Math.random() | 0]);
+    player.pos.y = 0;
+    player.pos.x = (arena[0].length / 2 | 0) - (player.matrix[0].length / 2 | 0);
+
+    if (collide(arena, player)) {
+        arena.forEach(row => row.fill(0));
+
+        player.score = 0;
+    }
+}
+
+function rotate(matrix, dir) {
+    for (let y = 0; y < matrix.length; ++y) {
+        for (let x = 0; x < y; ++x) {
+            [
+                matrix[x][y],
+                matrix[y][x]
+            ] = [
+                matrix[y][x],
+                matrix[x][y]
+            ];
+        }
+    }
+    if (dir > 0) {
+        matrix.forEach(row => row.reverse());
+    } else {
+        matrix.reverse();
+    }
+}
+
+function playerRotate(dir) {
+    const pos  = player.pos.x;
+    let offset = 1;
+    rotate(player.matrix, dir);
+    while (collide(arena, player)) {
+        player.pos.x += offset;
+        offset = -(offset + (offset > 0 ? 1 : -1));
+        if (offset > player.matrix[0].length) {
+            rotate(player.matrix, -dir);
+            player.pos.x = pos;
+            return;
+        }
+    }
+}
+
 function playerDrop() {
     player.pos.y++;
     if (collide(arena, player)) {
         player.pos.y--;
         merge(arena, player);
-        player.pos.y = 0;
+        playerReset();
+        arenaSweep();
+        updateScore();
     }
     dropCounter = 0;
 }
@@ -91,12 +203,17 @@ document.addEventListener('keydown', event => {
         playerMove(1);
     } else if (event.keyCode === 40) {
         playerDrop();
+    } else if (event.keyCode === 81) {
+        playerRotate(-1);
+    } else if (event.keyCode === 87) {
+        playerRotate(1);
     }
 });
 
 const player = {
-    matrix: matrix,
-    pos: {x: 5, y: 5}
+    matrix: null,
+    pos: {x: 0, y: 0},
+    score: 0
 };
 
 let dropCounter = 0;
@@ -115,5 +232,24 @@ function update(time = 0) {
     draw();
     requestAnimationFrame(update);
 }
+
+function updateScore() {
+    document.getElementById('score').innerText = player.score;
+}
+
+const colors = [
+    null,
+    '#fac',
+    '#caf',
+    '#ccf',
+    '#ced',
+    '#cfe',
+    '#cec',
+    '#fcd'
+];
+
+playerReset();
+
+updateScore();
 
 update();
