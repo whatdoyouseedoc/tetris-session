e88f04fdd95e5f37fa15422d80ff6ed1cf566cab
Alexander <whatdoyouseedoc@gmail.com>
2018-02-04 15:14:20 +0300

    Refactored to OOP approach. Implemented two player mode;

--- tetris.js
+++ tetris.js
@@ -1,36 +1,3 @@
-const canvas = document.getElementById('canvas');
-const context = canvas.getContext('2d');
-
-context.fillStyle = '#000';
-context.fillRect(0, 0, canvas.width, canvas.height);
-context.scale(20, 20);
-
-function arenaSweep() {
-    let rowCount = 1;
-    
-    outer: for (let y = arena.length - 1; y > 0 ; --y) {
-        for (let x = 0; x < arena[y].length; ++x) {
-            if (arena[y][x] === 0) {
-                continue outer;
-            }
-        }
-
-        const row = arena.splice(y, 1)[0].fill(0);
-        arena.unshift(row);
-        ++y;
-        player.score += rowCount * 10;
-        rowCount *= 2;
-    }
-}
-
-function createMatrix(w, h) {
-    const matrix = [];
-    while (h--) {
-        matrix.push(new Array(w).fill(0));
-    }
-    return matrix;
-}
-
 const pieces = {
     'T': [
         [0, 0, 0],
@@ -69,6 +36,13 @@ const pieces = {
     ],
 };
 
+const tetri = [];
+const playerElements = document.querySelectorAll('.player');
+[...playerElements].forEach(element => {
+    const tetris = new Tetris(element);
+    tetri.push(tetris);
+});
+
 function createPiece(type) {
     switch(type) {
         case 'T': return pieces.T; break;
@@ -82,174 +56,35 @@ function createPiece(type) {
     }
 }
 
-const arena = createMatrix(12, 20);
-
-function merge(arena, player) {
-    player.matrix.forEach((row, y) => {
-        row.forEach((value, x) => {
-            if (value !== 0) {
-                arena[y + player.pos.y][x + player.pos.x] = value;
+const keyhandlers = (event) => {
+    [
+        [65, 68, 81, 69, 83],
+        [72, 75, 89, 73, 74]
+    ].forEach((key, index) => {
+        const player = tetri[index].player;
+        if (event.type === 'keydown') {
+            if (event.keyCode === key[0]) {
+                player.move(-1);
+            } else if (event.keyCode === key[1]) {
+                player.move(1);
+            } else if (event.keyCode === key[2]) {
+                player.rotate(-1);
+            } else if (event.keyCode === key[3]) {
+                player.rotate(1);
             }
-        });
+        }
+        if (event.keyCode === key[4]) {
+            if (event.type === 'keydown') {
+                if (player.dropInterval !== player.DROP_FAST) {
+                    player.drop();
+                    player.dropInterval = player.DROP_FAST;
+                }
+            } else {
+                player.dropInterval = player.DROP_SLOW;
+            }
+        } 
     });
-}
-
-function collide(arena, player) {
-    const [m, o] = [player.matrix, player.pos];
-
-    for (let y = 0; y < m.length; ++y) {
-        for (let x = 0; x < m[y].length; ++x) {
-            if (m[y][x] !== 0 &&
-                (arena[y + o.y] && arena[y + o.y][x + o.x]) !== 0) {
-                    return true;
-            }
-        }
-    }
-    return false;
-}
-
-function clearStage() {
-    context.fillStyle = '#000';
-    context.fillRect(0, 0, canvas.width, canvas.height);
-}
-
-function draw() {
-    clearStage();
-    drawMatrix(arena, {x: 0, y: 0});
-    drawMatrix(player.matrix, player.pos);
-}
-
-function drawMatrix(matrix, offset) {
-    matrix.forEach((row, y) => {
-        row.forEach((value, x) => {
-            if (value !== 0) {
-                context.fillStyle = colors[value];
-                context.fillRect(x + offset.x, y + offset.y, 1, 1);
-            }
-        });
-    });
-}
-
-function playerMove(dir) {
-    player.pos.x += dir;
-    if (collide(arena, player)) {
-        player.pos.x -= dir;
-    }
-}
-
-function playerReset() {
-    const pcs = 'OILJSZT';
-    player.matrix = createPiece(pcs[pcs.length * Math.random() | 0]);
-    player.pos.y = 0;
-    player.pos.x = (arena[0].length / 2 | 0) - (player.matrix[0].length / 2 | 0);
-
-    if (collide(arena, player)) {
-        arena.forEach(row => row.fill(0));
-
-        player.score = 0;
-    }
-}
-
-function rotate(matrix, dir) {
-    for (let y = 0; y < matrix.length; ++y) {
-        for (let x = 0; x < y; ++x) {
-            [
-                matrix[x][y],
-                matrix[y][x]
-            ] = [
-                matrix[y][x],
-                matrix[x][y]
-            ];
-        }
-    }
-    if (dir > 0) {
-        matrix.forEach(row => row.reverse());
-    } else {
-        matrix.reverse();
-    }
-}
-
-function playerRotate(dir) {
-    const pos  = player.pos.x;
-    let offset = 1;
-    rotate(player.matrix, dir);
-    while (collide(arena, player)) {
-        player.pos.x += offset;
-        offset = -(offset + (offset > 0 ? 1 : -1));
-        if (offset > player.matrix[0].length) {
-            rotate(player.matrix, -dir);
-            player.pos.x = pos;
-            return;
-        }
-    }
-}
-
-function playerDrop() {
-    player.pos.y++;
-    if (collide(arena, player)) {
-        player.pos.y--;
-        merge(arena, player);
-        playerReset();
-        arenaSweep();
-        updateScore();
-    }
-    dropCounter = 0;
-}
-
-document.addEventListener('keydown', event => {
-    if (event.keyCode === 37) {
-        playerMove(-1);
-    } else if (event.keyCode === 39) {
-        playerMove(1);
-    } else if (event.keyCode === 40) {
-        playerDrop();
-    } else if (event.keyCode === 81) {
-        playerRotate(-1);
-    } else if (event.keyCode === 87) {
-        playerRotate(1);
-    }
-});
-
-const player = {
-    matrix: null,
-    pos: {x: 0, y: 0},
-    score: 0
 };
 
-let dropCounter = 0;
-let dropInterval = 1000;
-
-let lastTime = 0;
-function update(time = 0) {
-    const deltaTime = time - lastTime;
-    lastTime = time;
-
-    dropCounter += deltaTime;
-    if (dropCounter > dropInterval) {
-        playerDrop();
-    }
-
-    draw();
-    requestAnimationFrame(update);
-}
-
-function updateScore() {
-    document.getElementById('score').innerText = player.score;
-}
-
-const colors = [
-    null,
-    '#fac',
-    '#caf',
-    '#ccf',
-    '#ced',
-    '#cfe',
-    '#cec',
-    '#fcd'
-];
-
-playerReset();
-
-updateScore();
-
-update();
+document.addEventListener('keydown', keyhandlers);
+document.addEventListener('keyup', keyhandlers);
